b543905d797c738f35e256cc23e1ecb82d7bc396
kogisin <jaybkim2@gmail.com>
2021-04-22 16:47:23 +0900

    WIP: liquidity transaction messages

---
+++ transaction/transaction.go
@@ -0,0 +1,26 @@
+package transaction
+
+// MsgCreatePool
+func MsgCreatePool() {
+
+}
+
+// MsgDeposit
+func MsgDeposit() {
+
+}
+
+// MsgWithdraw
+func MsgWithdraw() {
+
+}
+
+// MsgSwap
+func MsgSwap() {
+
+}
+
+// WrapMultiMsgs
+func WrapMultiMsgs() {
+
+}
