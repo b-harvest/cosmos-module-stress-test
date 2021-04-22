f7211994467f30bdaa6b422fbf45786d09b4e582
kogisin <jaybkim2@gmail.com>
2021-04-22 15:13:05 +0900

    add unit tests for config

---
+++ config/config_test.go
@@ -0,0 +1,41 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/b-harvest/liquidity-stress-test/config"
+)
+
+func TestReadConfigFile(t *testing.T) {
+	configFilePath := "../config.toml"
+
+	cfg, err := config.Read(configFilePath)
+	require.NoError(t, err)
+
+	require.Equal(t, "http://localhost:26657", cfg.RPC.Address)
+	require.Equal(t, "localhost:9090", cfg.GRPC.Address)
+	require.Equal(t, "http://localhost:1317", cfg.LCD.Address)
+}
+
+func TestParseConfigString(t *testing.T) {
+	var sampleConfig = `
+[rpc]
+address = "http://localhost:26657"
+
+[grpc]
+address = "localhost:9090"
+insecure = true
+
+[lcd]
+address = "http://localhost:1317"
+`
+
+	cfg, err := config.ParseString([]byte(sampleConfig))
+	require.NoError(t, err)
+
+	require.Equal(t, "http://localhost:26657", cfg.RPC.Address)
+	require.Equal(t, "localhost:9090", cfg.GRPC.Address)
+	require.Equal(t, "http://localhost:1317", cfg.LCD.Address)
+}
