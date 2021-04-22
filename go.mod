7d4dbcca2ba5d7d75599a958ffb76ee41297b83c
kogisin <jaybkim2@gmail.com>
2021-04-22 15:02:20 +0900

    initialize go mod

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/b-harvest/liquidity-stress-test
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Stress Test Program for the liquidity module !")
+}
