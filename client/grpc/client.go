7af74e004b8445a890ac6729dd253322c035c820
kogisin <jaybkim2@gmail.com>
2021-04-28 21:22:16 +0900

    increase grpc timeout

--- client/grpc/client.go
+++ client/grpc/client.go
@@ -17,7 +17,7 @@ type Client struct {
 
 // NewClient creates GRPC client.
 func NewClient(grpcURL string, timeout int64) (*Client, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	client, err := grpc.DialContext(ctx, grpcURL, grpc.WithInsecure(), grpc.WithBlock())
