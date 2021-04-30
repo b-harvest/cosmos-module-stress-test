508579db7c49275cbfb187a7ed9dfd1b31a1607b
kogisin <jaybkim2@gmail.com>
2021-05-01 08:46:27 +0900

    add grpc query service for getting balance of an account

---
+++ client/grpc/bank.go
@@ -0,0 +1,30 @@
+package grpc
+
+import (
+	"context"
+
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+)
+
+// GetBankQueryClient returns a object of queryClient.
+func (c *Client) GetBankQueryClient() banktypes.QueryClient {
+	return banktypes.NewQueryClient(c)
+}
+
+// GetBalance returns balance of a given account for staking denom.
+func (c *Client) GetBalance(ctx context.Context, address string, denom string) (*sdktypes.Coin, error) {
+	bankClient := c.GetBankQueryClient()
+
+	req := banktypes.QueryBalanceRequest{
+		Address: address,
+		Denom:   denom,
+	}
+
+	resp, err := bankClient.Balance(ctx, &req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.GetBalance(), nil
+}
