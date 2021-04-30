a5f3614b4199f901de1147ef26a47059ceaa0821
kogisin <jaybkim2@gmail.com>
2021-05-01 08:40:45 +0900

    add grpc query service for getting pool information

--- client/grpc/pools.go
+++ client/grpc/pools.go
@@ -13,6 +13,22 @@ func (c *Client) GetLiquidityQueryClient() liquiditytypes.QueryClient {
 	return liquiditytypes.NewQueryClient(c)
 }
 
+// GetPool returns pool information.
+func (c *Client) GetPool(ctx context.Context, poolId uint64) (liquiditytypes.Pool, error) {
+	client := c.GetLiquidityQueryClient()
+
+	req := liquiditytypes.QueryLiquidityPoolRequest{
+		PoolId: poolId,
+	}
+
+	resp, err := client.LiquidityPool(ctx, &req)
+	if err != nil {
+		return liquiditytypes.Pool{}, err
+	}
+
+	return resp.GetPool(), nil
+}
+
 // GetAllPools returns all existing pools.
 func (c *Client) GetAllPools(ctx context.Context) (liquiditytypes.Pools, error) {
 	client := c.GetLiquidityQueryClient()
