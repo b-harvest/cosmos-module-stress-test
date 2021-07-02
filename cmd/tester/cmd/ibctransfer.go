d48ad5e767f408065fab99433c58eff5a81f8ca8
nodebreaker0-0 <zeroday26@gmail.com>
2021-07-03 03:11:36 +0900

    seq update

--- cmd/tester/cmd/ibctransfer.go
+++ cmd/tester/cmd/ibctransfer.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/b-harvest/cosmos-module-stress-test/client"
 	"github.com/b-harvest/cosmos-module-stress-test/config"
 	"github.com/b-harvest/cosmos-module-stress-test/tx"
 	"github.com/b-harvest/cosmos-module-stress-test/wallet"
+	rpcclient "github.com/tendermint/tendermint/rpc/client"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	sdktypes "github.com/cosmos/cosmos-sdk/types"
@@ -28,7 +30,7 @@ const (
 
 func IBCtransferCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "transfer [src-port] [src-channel] [receiver] [amount] [round] [tx-num] [msg-num]",
+		Use:     "transfer [src-port] [src-channel] [receiver] [amount] [blocks] [tx-num] [msg-num]",
 		Short:   "Transfer a fungible token through IBC",
 		Aliases: []string{"t"},
 		Args:    cobra.ExactArgs(7),
@@ -36,7 +38,7 @@ func IBCtransferCmd() *cobra.Command {
 
 Example: $tester t transfer channel-0 cosmos1pacc0fr45hggcn8jrfhgnqf8vgyqna7r5sftql 10uatom 10 1 1
 
-round: how many rounds to run
+blocks: how many blocks to keep the test going?
 tx-num: how many transactions to be included in a block
 msg-num: how many transaction messages to be included in a transaction
 `,
@@ -83,9 +85,9 @@ msg-num: how many transaction messages to be included in a transaction
 				coin.Denom = denomTrace.IBCDenom()
 			}
 
-			round, err := strconv.Atoi(args[4])
+			blocks, err := strconv.Atoi(args[4])
 			if err != nil {
-				return fmt.Errorf("round must be integer: %s", args[0])
+				return fmt.Errorf("blocks must be integer: %s", args[0])
 			}
 
 			txNum, err := strconv.Atoi(args[5])
@@ -109,48 +111,91 @@ msg-num: how many transaction messages to be included in a transaction
 
 			tx := tx.IbcNewtransaction(client, chainID, gasLimit, fees, memo)
 
-			for i := 0; i < round; i++ {
-				var txBytes [][]byte
+			account, err := client.GRPC.GetBaseAccountInfo(ctx, accAddr)
+			if err != nil {
+				return fmt.Errorf("failed to get account information: %s", err)
+			}
+			accSeq := account.GetSequence()
+			accNum := account.GetAccountNumber()
+			blockTimes := make(map[int64]time.Time)
+			st, err := client.RPC.Status(ctx)
+			if err != nil {
+				return fmt.Errorf("get status: %w", err)
+			}
+			startingHeight := st.SyncInfo.LatestBlockHeight + 2
+			log.Info().Msgf("current block height is %d, waiting for the next block to be committed", st.SyncInfo.LatestBlockHeight)
 
-				account, err := client.GRPC.GetBaseAccountInfo(ctx, accAddr)
+			if err := rpcclient.WaitForHeight(client.RPC, startingHeight-1, nil); err != nil {
+				return fmt.Errorf("wait for height: %w", err)
+			}
+			log.Info().Msgf("starting simulation #%d, blocks = %d, num txs per block = %d", blocks+1, blocks, txNum)
+			targetHeight := startingHeight
+
+			for i := 0; i < blocks; i++ {
+				st, err := client.RPC.Status(ctx)
 				if err != nil {
-					return fmt.Errorf("failed to get account information: %s", err)
+					return fmt.Errorf("get status: %w", err)
+				}
+				if st.SyncInfo.LatestBlockHeight != targetHeight-1 {
+					log.Warn().Int64("expected", targetHeight-1).Int64("got", st.SyncInfo.LatestBlockHeight).Msg("mismatching block height")
+					targetHeight = st.SyncInfo.LatestBlockHeight + 1
 				}
 
-				accSeq := account.GetSequence()
-				accNum := account.GetAccountNumber()
+				started := time.Now()
+				sent := 0
+			loop:
+				for sent < txNum {
+					msgs, err := tx.CreateTransferBot(cmd, ibcclientCtx, srcPort, srcChannel, coin, accAddr, receiver, msgNum)
+					if err != nil {
+						return fmt.Errorf("failed to create msg: %s", err)
+					}
+					for sent < txNum {
+						txByte, err := tx.IbcSign(ctx, accSeq, accNum, privKey, msgs...)
+						if err != nil {
+							return fmt.Errorf("failed to sign and broadcast: %s", err)
+						}
+						resp, err := client.GRPC.BroadcastTx(ctx, txByte)
+						//log.Info().Msgf("took %s broadcasting txs", resp)
+						if err != nil {
+							return fmt.Errorf("broadcast tx: %w", err)
+						}
+						accSeq = accSeq + 1
+						if resp.TxResponse.Code != 0 {
+							if resp.TxResponse.Code == 0x14 {
+								log.Warn().Msg("mempool is full, stopping")
+								accSeq = accSeq - 1
+								break loop
+							}
+						}
+						sent++
+					}
+				}
+				log.Debug().Msgf("took %s broadcasting txs", time.Since(started))
 
-				msgs, err := tx.CreateTransferBot(cmd, ibcclientCtx, srcPort, srcChannel, coin, accAddr, receiver, msgNum)
-
+				if err := rpcclient.WaitForHeight(client.RPC, targetHeight, nil); err != nil {
+					return fmt.Errorf("wait for height: %w", err)
+				}
+				r, err := client.RPC.Block(ctx, &targetHeight)
 				if err != nil {
-					return fmt.Errorf("failed to create msg: %s", err)
+					return err
 				}
-
-				for _, msg := range msgs {
-					fmt.Println("msg: ", msg)
+				var blockDuration time.Duration
+				bt, ok := blockTimes[targetHeight-1]
+				if !ok {
+					log.Warn().Msg("past block time not found")
+				} else {
+					blockDuration = r.Block.Time.Sub(bt)
+					delete(blockTimes, targetHeight-1)
 				}
-
-				for i := 0; i < txNum; i++ {
-					txByte, err := tx.IbcSign(ctx, accSeq, accNum, privKey, msgs...)
-					if err != nil {
-						return fmt.Errorf("failed to sign and broadcast: %s", err)
-					}
-
-					accSeq = accSeq + 1
-
-					txBytes = append(txBytes, txByte)
-				}
-
-				log.Info().Msgf("round:%d; txNum:%d; msgNum: %d; accAddr:%s", i+1, txNum, msgNum, accAddr)
-
-				for _, txByte := range txBytes {
-					resp, err := client.GRPC.BroadcastTx(ctx, txByte)
-					if err != nil {
-						return fmt.Errorf("failed to broadcast transaction: %s", err)
-					}
-
-					log.Info().Msgf("%s/cosmos/tx/v1beta1/txs/%s", cfg.LCD.Address, resp.TxResponse.TxHash)
-				}
+				blockTimes[targetHeight] = r.Block.Time
+				log.Info().
+					Int64("height", targetHeight).
+					Str("block-time", r.Block.Time.Format(time.RFC3339Nano)).
+					Str("block-duration", blockDuration.String()).
+					Int("broadcast-txs", sent).
+					Int("committed-txs", len(r.Block.Txs)).
+					Msg("block committed")
+				targetHeight++
 			}
 
 			return nil
